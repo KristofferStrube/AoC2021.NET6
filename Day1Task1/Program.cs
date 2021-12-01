5d9cd4b87f84e03d4b3192e2e41f4c9b96166cbd
Kristoffer Strube <kristoffer.strube@gmail.com>
2021-12-01 18:21:09 +0100

    Solutions for Day1

---
+++ Day1Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿IEnumerable<string> Reads()
+{
+    while (true)
+        yield return Console.ReadLine();
+}
+
+Console.WriteLine(
+    Reads()
+        .TakeWhile(s => !s.Equals(string.Empty))
+        .Select(s => int.Parse(s))
+        .Aggregate(
+            (prev: int.MaxValue, num: 0),
+            (acc, curr) =>
+            {
+                acc.num += curr > acc.prev ? 1 : 0;
+                acc.prev = curr;
+                return acc;
+            })
+        .num
+);
---
+++ Day1Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿IEnumerable<string> Reads()
+{
+    while (true)
+        yield return Console.ReadLine();
+}
+
+Console.WriteLine(
+    Reads()
+        .TakeWhile(s => !s.Equals(string.Empty))
+        .Select(s => int.Parse(s))
+        .Aggregate(
+            (rd: int.MaxValue, nd: int.MaxValue, st: int.MaxValue, num: 0),
+            (acc, curr) =>
+            {
+                acc.num += acc.nd + acc.st + curr > acc.rd + acc.nd + acc.st ? 1 : 0;
+                (acc.rd, acc.nd, acc.st) = (acc.nd, acc.st, curr);
+                return acc;
+            })
+        .num - 1
+);

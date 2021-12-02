407e7a39482791a8a839a255dd8bfec53cd4baae
au591259 <au591259@uni.au.dk>
2021-12-02 18:45:23 +0100

    Solutions for Day2

---
+++ Day2Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿IEnumerable<string> Reads()
+{
+    while (true)
+        yield return Console.ReadLine();
+}
+
+
+var pos = Reads()
+    .TakeWhile(s => !s.Equals(string.Empty))
+    .Select(s => (d: s[0], c: int.Parse(s.Split(" ")[1])))
+    .Aggregate(
+        (x: 0, y: 0),
+        (pos, curr) =>
+            curr.d == 'u' ? (pos.x, pos.y - curr.c) :
+            curr.d == 'd' ? (pos.x, pos.y + curr.c) :
+            (pos.x + curr.c, pos.y)
+    );
+
+Console.WriteLine(pos.x * pos.y);
---
+++ Day2Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿IEnumerable<string> Reads()
+{
+    while (true)
+        yield return Console.ReadLine();
+}
+
+
+var pos = Reads()
+    .TakeWhile(s => !s.Equals(string.Empty))
+    .Select(s => (d: s[0], c: int.Parse(s.Split(" ")[1])))
+    .Aggregate(
+        (x: 0, y: 0, aim: 0),
+        (prev, curr) =>
+            curr.d == 'd' ? (prev.x, prev.y, prev.aim + curr.c) :
+            curr.d == 'u' ? (prev.x, prev.y, prev.aim - curr.c) :
+            (prev.x + curr.c, prev.y + prev.aim * curr.c, prev.aim)
+    );
+
+Console.WriteLine(pos.x * pos.y);

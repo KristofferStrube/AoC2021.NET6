4da7a877577830efbce858bcd067e929e64db4ee
Kristoffer Strube <kristoffer.strube@gmail.com>
2021-12-03 07:02:03 +0100

    Solutions for Day3

---
+++ Day3Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using static Helpers.Helpers;
+
+var reads = Reads().ToList();
+
+Enumerable
+    .Range(0, reads[0].Length)
+    .Aggregate((gamma: "", epsilon: ""),
+    (res, i) =>
+        Enumerable
+            .Range(0, reads.Count)
+            .Count(j => reads[j][i] == '0')
+            >
+            reads.Count / 2
+            ?
+            (res.gamma + '0', res.epsilon + '1')
+            :
+            (res.gamma + '1', res.epsilon + '0'))
+    .Then(res => Convert.ToInt32(res.gamma, 2) * Convert.ToInt32(res.epsilon, 2))
+    .Then(Console.WriteLine);
---
+++ Day3Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using static Helpers.Helpers;
+
+var reads = Reads().ToList();
+
+var validGammas = Enumerable.Range(0, reads.Count).ToList();
+var validEpsilons = Enumerable.Range(0, reads.Count).ToList();
+
+for (int i = 0; i < reads[0].Length; i++)
+{
+    if (validGammas.Count(j => reads[j][i] == '0') > validGammas.Count() / 2)
+    {
+        validGammas = validGammas.Where(j => reads[j][i] == '0').ToList();
+    }
+    else
+    {
+        validGammas = validGammas.Where(j => reads[j][i] == '1').ToList();
+    }
+    if (validGammas.Count() == 1) break;
+}
+
+
+for (int i = 0; i < reads[0].Length; i++)
+{
+    if (validEpsilons.Count(j => reads[j][i] == '0') <= validEpsilons.Count() / 2)
+    {
+        validEpsilons = validEpsilons.Where(j => reads[j][i] == '0').ToList();
+    }
+    else
+    {
+        validEpsilons = validEpsilons.Where(j => reads[j][i] == '1').ToList();
+    }
+    if (validEpsilons.Count() == 1) break;
+}
+
+var gammaInt = Convert.ToInt32(reads[validGammas.First()], 2);
+var epsilonInt = Convert.ToInt32(reads[validEpsilons.First()], 2);
+
+Console.WriteLine(gammaInt * epsilonInt);
